dd378693d3dbeb42d66de8e8e1785e98ca69b131
Phillip Heller <pheller@me.com>
2022-12-29 18:06:59 -0700

    Log CMC messages, fixes #35
    - emits warning level log with data sent to CMC, formatted if possible
    - corresponding test
    - sending CMC logs to the database is still TBD.
    - increased test config log verbosity for CMC log capture test

--- apps/server/lib/prodigy/server/service/cmc.ex
+++ apps/server/lib/prodigy/server/service/cmc.ex
@@ -24,42 +24,65 @@ defmodule Prodigy.Server.Service.Cmc do
   alias Prodigy.Server.Session
   alias Prodigy.Server.Protocol.Dia.Packet.{Fm0, Fm9}
 
-  def handle(%Fm0{payload: %Fm9{payload: _payload}} = request, %Session{} = session) do
+  def handle(%Fm0{fm9: %Fm9{payload: payload}} = request, %Session{} = session) do
     Logger.debug("cmc got #{inspect(request, base: :hex, limit: :infinity)}")
 
-    # will have an FM9 header, not concatenated
-    # function code = 3 (alert)
-    # reason code = 2 (reception-oriented)
-    # flags = 0x90 (store by key, ascii)
-    # text length
+    # TODO insert the error details into the database
 
-    # architected text:
+    case payload do
+        <<
+          user_id::binary-size(7),          # right padded with '?'
+          "  ",
+          system_origin,                    # "T" = Trintex
+          msg_origin::binary-size(3),       # "PCM" = pcmessage
+          unit_id::binary-size(2),          # ascii decimals, "10" typical
+          error_code::binary-size(2),       # ascii decimals, "02" typical
+          severity_level,                   # 'E'
+          " ",
+          error_threshold::binary-size(3),  # "001"
+          " ",
+          date::binary-size(8),             # '05231988' typical
+          time::binary-size(6),             # '143023' typical
+          api_event::binary-size(5),        # '00003' typical
+          mem_to_start::binary-size(8),     # '00227472' typical
+          dos_version::binary-size(5),      # '03.30' typical
+          rs_version::binary-size(8),       # '6.01.XX' typical
+          window_id::binary-size(11),       # 'NOWINDOWIDX' typical
+          window_last::binary-size(2),      # in ascii hex, '0104' typical
+          selected_id::binary-size(11),     # 'NOSELECTORX' typical
+          selected_last::binary-size(2),    # '0104 typical
+          base_id::binary-size(11),         # 'PIOT0010MAP' typical
+          base_last::binary-size(2),        # '0104' typical
+          keyword::binary-size(13)          # 'QUOTE TRACK  ' typical
+        >> ->
+          msg = """
+          CMC FM9 Error reported by Reception System
 
-    #    <<
-    #      user_id::binary-size(7),          # right padded with '?'
-    #      "  ",
-    #      system_origin,                    # "T" = Trintex
-    #      msg_origin::binary-size(3),       # "PCM" = pcmessage
-    #      unit_id::binary-size(2),          # ascii decimals, "10" typical
-    #      error_code::binary-size(2),       # ascii decimals, "02" typical
-    #      severity_level,                   # 'E'
-    #      " ",
-    #      error_threshold::binary-size(3),  # "001"
-    #      " ",
-    #      date::binary-size(8),             # '05231988' typical
-    #      time::binary-size(6),             # '143023' typical
-    #      api_event::binary-size(5),        # '00003' typical
-    #      mem_to_start::binary-size(8),     # '00227472' typical
-    #      dos_version::binary-size(5),      # '03.30' typical
-    #      rs_version::binary-size(8),       # '6.01.XX' typical
-    #      window_id::binary-size(11),       # 'NOWINDOWIDX' typical
-    #      window_last::binary-size(2),      # in ascii hex, '0104' typical
-    #      selected_id::binary-size(11),     # 'NOSELECTORX' typical
-    #      selected_last::binary-size(4),    # '0104 typical
-    #      base_id::binary-size(11),         # 'PIOT0010MAP' typical
-    #      base_last::binary-size(2),        # '0104' typical
-    #      keyword::binary-size(13)          # 'QUOTE TRACK  ' typical
-    #    >>
+                  User ID: #{user_id}
+            System Origin: #{system_origin}
+           Message Origin: #{msg_origin}
+                  Unit ID: #{unit_id}
+               Error Code: #{error_code}
+           Severity Level: #{severity_level}
+          Error Threshold: #{error_threshold}
+                     Date: #{date}
+                     Time: #{time}
+                API Event: #{api_event}
+             Starting RAM: #{mem_to_start}
+              DOS Version: #{dos_version}
+               RS Version: #{rs_version}
+                Window ID: #{window_id}
+              Window Last: #{inspect window_last, base: :hex}
+              Selected ID: #{selected_id}
+            Selected Last: #{inspect selected_last, base: :hex}
+                  Base ID: #{base_id}
+                Base Last: #{inspect base_last, base: :hex}
+                  Keyword: #{keyword}
+          """
+          Logger.warn(msg)
+        _ ->
+          Logger.warn("CMC received message in unknown format: #{inspect payload, base: :hex, limit: :infinity}")
+      end
 
     {:ok, session, <<>>}
   end
